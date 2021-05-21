be186361b94c48203358ba70b2421bccfbd4348b
Accup <32096121+accup@users.noreply.github.com>
2021-05-22 01:03:07 +0900

    [update] 実装をWebGLに変更（パフォーマンスの最適化）

--- main.js
+++ main.js
@@ -1,28 +1,128 @@
-/**
- * @template T
- * @param {HTMLElement} parent 
- * @param {string} tag
- * @param {T[]} data
- * @param {(element: HTMLElement, datum: T) => void} update 
- */
-function updateElements(parent, tag, data, update) {
-    const elements = parent.children;
-    const oldLength = elements.length;
-    const newLength = data.length;
-    for (let index = oldLength; index < newLength; ++index) {
-        parent.appendChild(document.createElement(tag));
+// 頂点シェーダ（頂点）
+const vsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec2 aVertexTextureCoord;
+
+    uniform mat4 uMatrix;
+    uniform vec4 uColor;
+
+    varying lowp vec4 vColor;
+    varying lowp vec2 vTextureCoord;
+
+    void main() {
+        gl_Position = uMatrix * aVertexPosition;
+        vColor = uColor;
+        vTextureCoord = aVertexTextureCoord;
     }
-    for (let index = oldLength - 1; index >= newLength; --index) {
-        elements[index].style.display = 'none';
-        // parent.removeChild(elements[index]);
+`;
+
+// フラグメントシェーダ（色）
+const fsSource = `
+    varying lowp vec4 vColor;
+    varying lowp vec2 vTextureCoord;
+
+    void main() {
+        if (distance(vTextureCoord, vec2(0.5, 0.5)) <= 0.5) {
+            gl_FragColor = vColor;
+        } else {
+            discard;
+        }
+    }
+`;
+
+function orthoTranslateScaleMatrix(sx, sy, sz, dx, dy, dz, vw, vh) {
+    const matrix = new Float32Array(16);
+    matrix[0] = 2 * sx / vw; // 11
+    matrix[5] = 2 * sy / vh; // 22
+    matrix[10] = -sz;        // 33
+    matrix[12] = 2 * dx / vw; // 14
+    matrix[13] = 2 * dy / vh; // 24
+    matrix[14] = -dz;        // 34
+    matrix[15] = 1;          // 44
+    return matrix;
+}
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl
+ * @param {number} type
+ * @param {string} source
+ * @returns {WebGLShader?}
+ */
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        gl.deleteShader(shader);
+        return null;
     }
 
-    const newElements = parent.children;
-    for (let index = 0; index < newLength; ++index) {
-        newElements[index].style.removeProperty('display');
-        update(newElements[index], data[index]);
-    }
+    return shader;
 }
+
+
+/**
+ * @param {WebGLRenderingContext} gl
+ * @param {string} vsSource
+ * @param {string} fsSource
+ * @returns {WebGLProgram?}
+ */
+function initShaderProgram(gl, vsSource, fsSource) {
+    // 頂点シェーダ
+    const vShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    if (vShader === null) {
+        return null;
+    }
+    // フラグメントシェーダ
+    const fShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    if (fShader === null) {
+        gl.deleteShader(fShader);
+        return null;
+    }
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vShader);
+    gl.attachShader(shaderProgram, fShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        gl.deleteShader(vShader);
+        gl.deleteShader(fShader);
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+
+/**
+ * 実装依存なWebGLの初期処理
+ * @param {HTMLCanvasElement} canvas
+ * @returns {{gl: WebGLRenderingContext, shader: WebGLProgram}?}
+ */
+function initWebGL(canvas) {
+    const gl = canvas.getContext('webgl');
+    if (gl === null) {
+        return null;
+    }
+
+    // シェーダを構築
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    if (shaderProgram === null) {
+        gl.d
+        return null;
+    }
+
+    return {
+        gl: gl,
+        shader: shaderProgram,
+    };
+}
+
 
 /**
  * 
@@ -56,15 +156,31 @@ function initButtonGroup(element, listener) {
     onChange();
 }
 
+
+const deactivatedColor = [0.5, 0.5, 0.5, 0.1];
+const trackColors = [
+    [0.5, 1.0, 0.5, 0.2],
+    [0.5, 0.5, 1.0, 0.2],
+    [1.0, 0.5, 0.5, 0.2],
+    [0.5, 1.0, 1.0, 0.2],
+    [0.8, 0.5, 0.5, 0.2],
+    [1.0, 0.5, 1.0, 0.2],
+    [1.0, 1.0, 0.5, 0.2],
+];
+
+
 window.addEventListener('load', e => {
     const app = document.getElementById('app');
     const melodyPanel = document.getElementById('melody-panel');
+    /** @type {HTMLCanvasElement} */
+    const melodyPanelCanvas = document.getElementById('melody-panel-canvas');
     const midiFileDrop = document.getElementById('midi-file-drop');
     const fileNameLabel = document.getElementById('file-name-label');
     const playButton = document.getElementById('play-button');
     const changeCircleModeButtonGroup = document.getElementById('change-circle-mode-button-group');
 
     let midi = null;
+    let duration = 1.0;
     let tracks = [];
     let state = {
         mode: '12 semitones',
@@ -85,8 +201,6 @@ window.addEventListener('load', e => {
             tracks = [];
 
             midi.tracks.forEach(track => {
-                const div = document.createElement('div');
-
                 const volume = new Tone.Volume(-12).toDestination();
                 const synth = new Tone.PolySynth({
                     // maxPolyphony: 100,
@@ -123,10 +237,12 @@ window.addEventListener('load', e => {
                     offsets: {
                         off: 0,
                         on: 0,
+                        active: 0,
                     },
                 });
-                melodyPanel.appendChild(div);
             });
+
+            duration = Math.max(1.0, midi.duration);
 
             // Initialization
             function init() {
@@ -140,7 +256,7 @@ window.addEventListener('load', e => {
             function replay() {
                 Tone.Transport.seconds = 0.0;
             }
-            Tone.Transport.schedule(replay, midi.duration);
+            Tone.Transport.schedule(replay, duration);
         });
     }
 
@@ -175,61 +291,199 @@ window.addEventListener('load', e => {
         state.mode = value;
     });
 
-    requestAnimationFrame(function animationLoop() {
-        const currentTime = Tone.Transport.seconds;
-        const scale = 200;
+    const glinfo = initWebGL(melodyPanelCanvas);
+    if (glinfo === null) {
+        melodyPanel.textContent = "WebGLがサポートされていません。";
+    } else {
+        const gl = glinfo.gl;
 
-        let circleFactor;
-        switch (state.mode) {
-            case '12 semitones':
-                circleFactor = 1;
-                break;
-            case '24 semitones':
-                circleFactor = 0.5;
-                break;
-            case '48 semitones':
-                circleFactor = 0.25;
-                break;
-            case '96 semitones':
-                circleFactor = 0.125;
-                break;
-            case 'circle of fifths':
-                circleFactor = 7;
-                break;
+        function resizeCanvas() {
+            gl.canvas.width = gl.canvas.clientWidth;
+            gl.canvas.height = gl.canvas.clientHeight;
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         }
+        resizeCanvas();
 
-        updateElements(melodyPanel, 'div', tracks, (element, track) => {
-            const length = track.notes.length;
-            for (; track.offsets.on < length; ++track.offsets.on) {
-                const note = track.notes[track.offsets.on];
-                if (currentTime + 3 < note.time) break;
+        // ウィンドウサイズに合わせたキャンバスのリサイズ
+        let resizeTimerId = null;
+        window.addEventListener('resize', e => {
+            if (resizeTimerId !== null) {
+                clearTimeout(resizeTimerId);
             }
-            for (; track.offsets.off < length; ++track.offsets.off) {
-                const note = track.notes[track.offsets.off];
-                if (currentTime < note.time + note.duration) break;
-            }
-            const notes = track.notes.slice(track.offsets.off, track.offsets.on);
-
-            updateElements(element, 'div', notes, (element, note) => {
-                const offset = currentTime - note.time;
-                const theta = 2 * Math.PI * note.midi * circleFactor / 12;
-                const x = scale * (-offset + 0.5 * note.duration) * Math.cos(theta);
-                const y = scale * (-offset + 0.5 * note.duration) * -Math.sin(theta);
-
-                element.style.width = `${scale * note.duration}px`;
-                element.style.height = `${scale * note.duration}px`;
-                element.style.margin = `${-0.5 * scale * note.duration}px`;
-                element.style.transform = `translate(${x}px, ${y}px)`;
-                element.classList.toggle(
-                    'activated',
-                    0 <= offset && offset < note.duration,
-                );
-                if (note.duration <= offset) {
-                    element.style.display = 'none';
-                }
-            });
+            resizeTimerId = setTimeout(() => {
+                resizeCanvas();
+                resizeTimerId = null;
+            }, 50);
         });
 
-        requestAnimationFrame(animationLoop);
-    });
+        // アルファブレンドを有効化
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+        // 塗りつぶし色を黒に設定
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        const matrixUniform = gl.getUniformLocation(glinfo.shader, 'uMatrix');
+        const colorUniform = gl.getUniformLocation(glinfo.shader, 'uColor');
+        const vertexPositionAttribute = gl.getAttribLocation(glinfo.shader, 'aVertexPosition');
+        const vertexTextureCoordAttribute = gl.getAttribLocation(glinfo.shader, 'aVertexTextureCoord');
+
+        // 頂点位置バッファの設定
+        const positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([
+                -0.5, 0.5, 0.0, 1.0,
+                0.5, 0.5, 0.0, 1.0,
+                -0.5, -0.5, 0.0, 1.0,
+                0.5, -0.5, 0.0, 1.0,
+            ]),
+            gl.STATIC_DRAW,
+        );
+        gl.vertexAttribPointer(
+            vertexPositionAttribute,
+            4, // a number of float values per vertex position
+            gl.FLOAT, false, 0, 0)
+        gl.enableVertexAttribArray(vertexPositionAttribute);
+
+        // 頂点テクスチャ位置の設定
+        const textureCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([
+                0.0, 1.0,
+                1.0, 1.0,
+                0.0, 0.0,
+                1.0, 0.0,
+            ]),
+            gl.STATIC_DRAW,
+        );
+        gl.vertexAttribPointer(
+            vertexTextureCoordAttribute,
+            2, // a number of float values per vertex texture coord
+            gl.FLOAT, false, 0, 0)
+        gl.enableVertexAttribArray(vertexTextureCoordAttribute);
+
+        // シェーダの設定
+        gl.useProgram(glinfo.shader);
+
+        // アニメーションループ
+        requestAnimationFrame(function animationLoop() {
+            const width = gl.canvas.width;
+            const height = gl.canvas.height;
+
+            const currentTime = Tone.Transport.seconds;
+            const scale = 200;
+
+            let circleFactor;
+            switch (state.mode) {
+                case '12 semitones':
+                    circleFactor = 1;
+                    break;
+                case '24 semitones':
+                    circleFactor = 0.5;
+                    break;
+                case '48 semitones':
+                    circleFactor = 0.25;
+                    break;
+                case '96 semitones':
+                    circleFactor = 0.125;
+                    break;
+                case 'circle of fifths':
+                    circleFactor = 7;
+                    break;
+            }
+
+            // 画面を黒に塗りつぶし
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            // 必要なノートの描画
+            const trackLength = tracks.length;
+            for (let trackIndex = 0; trackIndex < trackLength; ++trackIndex) {
+                const track = tracks[trackIndex];
+                const noteLength = track.notes.length;
+                for (; track.offsets.off < noteLength; ++track.offsets.off) {
+                    const note = track.notes[track.offsets.off];
+                    if (currentTime < note.time + note.duration) break;
+                }
+                track.offsets.active = track.offsets.off;
+                for (; track.offsets.active < noteLength; ++track.offsets.active) {
+                    const note = track.notes[track.offsets.active];
+                    if (currentTime < note.time) break;
+                }
+                track.offsets.on = track.offsets.active;
+                for (; track.offsets.on < noteLength; ++track.offsets.on) {
+                    const note = track.notes[track.offsets.on];
+                    if (currentTime < note.time - 3) break;
+                }
+
+                // 色の設定（非アクティブ）
+                gl.uniform4fv(
+                    colorUniform,
+                    deactivatedColor,
+                );
+                for (let noteIndex = track.offsets.active; noteIndex < track.offsets.on; ++noteIndex) {
+                    const note = track.notes[noteIndex];
+                    const offset = currentTime - note.time;
+                    const theta = 2 * Math.PI * note.midi * circleFactor / 12;
+                    const dx = scale * (-offset + 0.5 * note.duration) * Math.cos(theta);
+                    const dy = scale * (-offset + 0.5 * note.duration) * -Math.sin(theta);
+                    const sw = scale * note.duration;
+                    const sh = scale * note.duration;
+
+                    if (offset < note.duration) {
+                        // ワールド兼射影行列の設定
+                        gl.uniformMatrix4fv(
+                            matrixUniform, false,
+                            orthoTranslateScaleMatrix(
+                                sw, sh, 1,
+                                dx, dy, 0,
+                                width, height,
+                            ),
+                        );
+                        gl.drawArrays(
+                            gl.TRIANGLE_STRIP,
+                            0,  // vertex offset
+                            4,  // a number of vertices
+                        );
+                    }
+                }
+
+                // 色の設定（アクティブ）
+                gl.uniform4fv(
+                    colorUniform,
+                    trackColors[trackIndex % trackColors.length],
+                );
+                for (let noteIndex = track.offsets.active - 1; noteIndex >= track.offsets.off; --noteIndex) {
+                    const note = track.notes[noteIndex];
+                    const offset = currentTime - note.time;
+                    const theta = 2 * Math.PI * note.midi * circleFactor / 12;
+                    const dx = scale * (-offset + 0.5 * note.duration) * Math.cos(theta);
+                    const dy = scale * (-offset + 0.5 * note.duration) * -Math.sin(theta);
+                    const sw = scale * note.duration;
+                    const sh = scale * note.duration;
+
+                    if (offset < note.duration) {
+                        // ワールド兼射影行列の設定
+                        gl.uniformMatrix4fv(
+                            matrixUniform, false,
+                            orthoTranslateScaleMatrix(
+                                sw, sh, 1,
+                                dx, dy, 0,
+                                width, height,
+                            ),
+                        );
+                        gl.drawArrays(
+                            gl.TRIANGLE_STRIP,
+                            0,  // vertex offset
+                            4,  // a number of vertices
+                        );
+                    }
+                }
+            }
+
+            requestAnimationFrame(animationLoop);
+        });
+    }
 });
